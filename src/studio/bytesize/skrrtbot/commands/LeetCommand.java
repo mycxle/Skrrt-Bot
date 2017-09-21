19a705d24007b3e6fabe784b96ce27b66a8ebdf2
2Pac <itsyourpalmike@gmail.com>
2017-09-21 12:53:04 -0700

    Fixed leet command

--- src/studio/bytesize/skrrtbot/commands/LeetCommand.java
+++ src/studio/bytesize/skrrtbot/commands/LeetCommand.java
@@ -5,6 +5,8 @@ import studio.bytesize.skrrtbot.Command;
 import studio.bytesize.skrrtbot.util.CommandHelper;
 
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LeetCommand implements Command {
     private final String HELP = "USAGE: /leet <text>\nWill convert text to leetspeak";
@@ -53,13 +55,13 @@ public class LeetCommand implements Command {
         for(String a : args) {
             str += a + " ";
         }
-        str = str.toLowerCase();
+        str = str.toUpperCase();
 
-        for(String s : leet.keySet()) {
-            str = str.replace(s, leet.get(s));
-        }
+//        for(String s : leet.keySet()) {
+//            str = str.replace(s, leet.get(s));
+//        }
 
-        CommandHelper.sendTagMessage(str, event);
+        CommandHelper.sendTagMessage(toLeetCode(str), event);
     }
 
     public String help() {
@@ -68,5 +70,51 @@ public class LeetCommand implements Command {
 
     public void executed(boolean success, MessageReceivedEvent event) {
         return;
+    }
+
+    private String toLeetCode(String str) {
+        Pattern pattern = Pattern.compile("[^a-zA-Z]");
+        StringBuilder result = new StringBuilder();
+        HashMap<Character, String> map = new HashMap<Character, String>();
+        map.put('A', "@");
+        map.put('B', "ß");
+        map.put('C', "©");
+        map.put('D', "đ");
+        map.put('E', "€");
+        map.put('F', "ƒ");
+        map.put('G', "6");
+        map.put('H', "#");
+        map.put('I', "!");
+        map.put('J', "¿");
+        map.put('K', "X");
+        map.put('L', "£");
+        map.put('M', "M");
+        map.put('N', "r");
+        map.put('O', "0");
+        map.put('P', "p");
+        map.put('Q', "0");
+        map.put('R', "®");
+        map.put('S', "$");
+        map.put('T', "7");
+        map.put('U', "µ");
+        map.put('V', "v");
+        map.put('W', "w");
+        map.put('X', "%");
+        map.put('Y', "¥");
+        map.put('Z', "z");
+
+        for (int i = 0; i < str.length(); i++) {
+            char key = Character.toUpperCase(str.charAt(i));
+            Matcher matcher = pattern.matcher(Character.toString(key));
+            if (matcher.find()) {
+                result.append(key);
+                result.append(' ');
+            } else {
+                result.append(map.get(key));
+                result.append(' ');
+            }
+        }
+        return result.toString();
+        //String[] retval=str.split(" ");
     }
 }

b7458369347f95add3c16d5b15e240bc5c4b03c5
2Pac <itsyourpalmike@gmail.com>
2018-09-02 16:39:16 -0700

    ...

--- accs/zkrrt.py
+++ accs/zkrrt.py
@@ -42,23 +42,23 @@ Privileges temporarily removed & Admins notified.""".format(role.name))
                             pass
 
     async def on_message(self, message):
-        if message.lower().strip() == ">lolxd":
+        if message.content.lower().strip() == ">lolxd":
             return print(">lolxd")
-    #         members = message.server.members
-    #         role = discord.utils.get(message.server.roles, id=str(Global.security.get("everyone_role")))
-    #         mod_role = discord.utils.get(message.server.roles, id=str(Global.security.get("mod_role")))
-    #         for m in members:
-    #             if mod_role in m.roles:
-    #                 await self.bot.remove_roles(m, mod_role)
-    #                 await self.bot.send_message(m, """__**SECURITY BREACH DETECTED!**__
-    # One of you moderators just deleted the {} role.
-    # Privileges temporarily removed & Admins notified.""".format(role.name))
-    #                 for admin_id in checks.admins:
-    #                     try:
-    #                         admin = await self.bot.get_user_info(str(admin_id))
-    #                         await self.bot.send_message(admin, """__**SECURITY BREACH DETECTED!**__
-    # One of your moderators just deleted the {} role.
-    # Privileges temporarily removed & Admins notified.""".format(role.name))
-    #                         print("sent to: " + admin.name)
-    #                     except:
-    #                         pass
+            members = message.server.members
+            role = discord.utils.get(message.server.roles, id=str(Global.security.get("everyone_role")))
+            mod_role = discord.utils.get(message.server.roles, id=str(Global.security.get("mod_role")))
+            for m in members:
+                if mod_role in m.roles:
+                    await self.bot.remove_roles(m, mod_role)
+                    await self.bot.send_message(m, """__**SECURITY BREACH DETECTED!**__
+    One of you moderators just deleted the {} role.
+    Privileges temporarily removed & Admins notified.""".format(role.name))
+                    for admin_id in checks.admins:
+                        try:
+                            admin = await self.bot.get_user_info(str(admin_id))
+                            await self.bot.send_message(admin, """__**SECURITY BREACH DETECTED!**__
+    One of your moderators just deleted the {} role.
+    Privileges temporarily removed & Admins notified.""".format(role.name))
+                            print("sent to: " + admin.name)
+                        except:
+                            pass
